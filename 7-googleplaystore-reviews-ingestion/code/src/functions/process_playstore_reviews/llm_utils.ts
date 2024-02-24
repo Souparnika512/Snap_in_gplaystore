98baa6fe295c5783ed601cf881915c66d0c1008a
Souparnika512 <souparnika.ks@siliziumcircuits.com>
2024-02-24 23:06:31 +0530

    snapin adobe

---
+++ 7-googleplaystore-reviews-ingestion/code/src/functions/process_playstore_reviews/llm_utils.ts
@@ -0,0 +1,38 @@
+import { StringOutputParser, JsonOutputParser } from "@langchain/core/output_parsers";
+import {
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} from "@langchain/core/prompts";
+import { Runnable } from "@langchain/core/runnables";
+import { ChatOpenAI } from "@langchain/openai";
+
+
+export class LLMUtils {
+  public provider!: Runnable;
+
+  // Constructor to initialize SDK instances
+  constructor(openAIApiKey: string, modelName: string, maxTokens: number) {
+    this.provider = new ChatOpenAI({
+      openAIApiKey: openAIApiKey,
+      modelName: modelName,
+      maxTokens: maxTokens,
+    }).bind({
+      response_format: {
+        type: "json_object",
+      },
+    });
+  }
+
+  // Chat completion.
+  async chatCompletion(sysPrompt: string, humanPrompt: string, argsValues: object): Promise<object> {
+    const chatPrompt = ChatPromptTemplate.fromMessages([
+      SystemMessagePromptTemplate.fromTemplate(sysPrompt),
+      HumanMessagePromptTemplate.fromTemplate(humanPrompt),
+    ]);
+    const outputParser = new JsonOutputParser();
+    const chain = chatPrompt.pipe(this.provider).pipe(outputParser);
+    const response = await chain.invoke(argsValues);
+    return response;
+  }
+}
